8c617ecad52957807cdb9eea12c661a1208906c6
AnailsonAraujo17 <anailsonara17@gmail.com>
2026-09-07 19:27:58 -0300

    docs: comentario funcao calcular total

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+function calcularTotal (itens) {
+    let total = o
+
+    for (let i = 0; < itens.length; i++){
+        total += itens [i].preco
+    {
+
+    // aplica desconto de fidelidade
+    // antes de retornar
+        return total
+}
