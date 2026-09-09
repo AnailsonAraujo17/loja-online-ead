a14ab3ab1adb41131671b6e417302c60a36830bd
AnailsonAraujo17 <anailsonara17@gmail.com>
2026-09-08 21:04:28 -0300

    adiciona logica de desconto

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function calcularTotal (itens) {
     let total = o
 
     for (let i = 0; < itens.length; i++){
-        total += itens [i].preco
+        total += itens [i].preco * 1.1
     {
 
     // aplica desconto de fidelidade
